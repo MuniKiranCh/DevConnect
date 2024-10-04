74397690299415f6994d5185fb81d25dfe01e42a
MuniKiranCh <125656756+MuniKiranCh@users.noreply.github.com>
2024-10-04 06:50:58 +0530

    added db file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 const express=require('express')
+const connectDB=require('./config/database')
 const app=express();
+const User=require("./models/user");
 
-app.use('/',(req,res)=>{
-    console.log(`Hello world`)
-    res.send('Hello world');
+app.post('/signup',async (req,res)=>{
+    const user=new User({
+        firstName:'Muni',
+        lastName:'Kiran',
+        emailId:'munikiranch@gmail.com',
+        password:'kiran@123'
+    });
+
+    await user.save();
+
+    res.send('User added successfully!')
 })
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000')
-})
+
+connectDB()
+    .then(()=>{
+    console.log(`Database connected successfully!`);
+        app.listen(3000,()=>{
+            console.log('server is running on port 3000')
+        })
+    })
+    .catch((err)=>{
+    console.log(`Database cannot be connected!`)
+    });
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose=require('mongoose');
+
+const connectDB=async ()=> {
+    await mongoose.connect("mongodb+srv://munikiran:Qwertycs12345@cluster0.vqw73pn.mongodb.net/devTinder");
+};
+
+module.exports=connectDB
