89ed6c6cc6c04dcb66d4358297111cbff421e556
MuniKiranCh <125656756+MuniKiranCh@users.noreply.github.com>
2024-10-04 15:44:23 +0530

    added apis

--- src/app.js
+++ src/app.js
@@ -5,10 +5,42 @@ const User=require("./models/user");
 const adminAuth=require('./middlewares/auth')
 
 app.use('/admin',adminAuth)
+app.use(express.json());
 
-app.get('/user',(req,res)=>{
-    console.log('Hi, this user route!');
-    res.send('Hello, user route 1');
+app.get('/user',async (req,res)=>{
+    const userEmail=req.body.emailId;
+
+    try{
+        const user=await User.findOne({emailId : userEmail})
+
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        else{
+            res.send(user);
+        }
+    }
+    catch(err){
+        res.status(400).send(`Something went wrong!`);
+    }
+})
+
+
+app.get('/feed',async (req,res)=>{
+    const userEmail=req.body.emailId;
+
+    try{
+        const users=await User.find({emailId : userEmail});
+
+        if(users.length===0){
+            return res.status(404).json({message:"User not found"});
+        }
+        else{
+            res.send(users);
+        }
+    }catch(err){
+        res.status(400).send(`Something went wrong!`);
+    }
 })
 
 app.get('/admin/getnames',(req,res)=>{
@@ -17,12 +49,15 @@ app.get('/admin/getnames',(req,res)=>{
 })
 
 app.post('/signup',async (req,res)=>{
-    const user=new User({
+    /*
+        {
         firstName:'Muni',
         lastName:'Kiran',
         emailId:'munikiranch@gmail.com',
         password:'kiran@123'
-    });
+        }
+    */
+    const user=new User(req.body);
 
     await user.save();
 
