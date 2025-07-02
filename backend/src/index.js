f5740b107d51f08a7f9ed36d2abbb7f0f480180f
ChittambakamMuniKiran <chittambakam.kiran@gep.com>
2025-07-03 02:48:40 +0530

    solved cors er

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,17 +18,19 @@ const allowedOrigins = [
   'https://dev-connect-olive.vercel.app',
   'http://localhost:5173'
 ];
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
-}));
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
+// }));
+
+app.use('*',cors());
 
 require('dotenv').config();
 app.use(express.json());
